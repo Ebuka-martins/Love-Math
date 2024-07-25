5f8a677c4b9ca93277e306a85a530a034f6e8fb2
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-07-25 15:19:25 +0000

    git: Display addition question to the users

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,34 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("you clicked submit");
         } else {
             let gameType = this.getAttribute("data-type");
-            alert(`you clicked ${gameType}`);
+           runGame(gameType);
         }
     })
   }
+
+  runGame("addition");
 })
+
+
 // create two random numbers between 1 and 25
-function runGames() {
-let num1 = Math.floor(Math.random() * 25) + 1;
-let num2 = Math.floor(Math.random() * 25) + 1;
+function runGame(gameType) {
+   let num1 = Math.floor(Math.random() * 25) + 1;
+   let num2 = Math.floor(Math.random() * 25) + 1;
+
+   if (gameType === "addition") {
+      displayAdditionQuestion(num1, num2);
+   } else {
+      alert (`unknown game type ${gameType}`);
+      throw `unknown game type ${gameType}. Aborting!`;
+   }
+
 }
 
+
+/**
+ * Get operand from numbers(minus, plus and subtraction)
+ * directly from DOM and returns the correct answer
+ */
 function checkAnswer() {
 
 }
@@ -36,8 +53,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
