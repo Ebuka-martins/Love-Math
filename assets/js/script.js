f176785541b3e64ee24463f3a56fa47aeefc2c01
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-07-25 18:05:27 +0000

    git: add code to check the answer and respond to the users

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,94 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  let buttons = document.getElementsByTagName('button');
+	let buttons = document.getElementsByTagName("button");
 
-  for (let button of buttons) {
-    button.addEventListener("click", function() {
-        if (this.getAttribute("data.type") === "submit") {
-            alert("you clicked submit");
-        } else {
-            let gameType = this.getAttribute("data-type");
-           runGame(gameType);
-        }
-    })
-  }
+	for (let button of buttons) {
+		button.addEventListener("click", function() {
+			if (this.getAttribute("data-type") === "submit") {
+				checkAnswer();
+			} else {
+				let gameType = this.getAttribute("data-type");
+				runGame(gameType);
+			}
+		});
+	}
 
-  runGame("addition");
-})
+	document.getElementById("answer-box").addEventListener("keydown", function(event) {
+		if (event.key === "Enter") {
+			checkAnswer();
+		}
+	});
+
+	runGame("addition");
+});
 
 
 // create two random numbers between 1 and 25
 function runGame(gameType) {
-   let num1 = Math.floor(Math.random() * 25) + 1;
-   let num2 = Math.floor(Math.random() * 25) + 1;
 
-   if (gameType === "addition") {
-      displayAdditionQuestion(num1, num2);
-   } else {
-      alert (`unknown game type ${gameType}`);
-      throw `unknown game type ${gameType}. Aborting!`;
-   }
+	// Generate two random numbers between 1 and 25
+	// Math.floor rounds down to the whole number
+	// Math.random generates random numbers
+
+	document.getElementById("answer-box").value = "";
+	document.getElementById("answer-box").focus();
+
+	let num1 = Math.floor(Math.random() * 25) + 1;
+	let num2 = Math.floor(Math.random() * 25) + 1;
+
+	if (gameType === "addition") {
+		displayAdditionQuestion(num1, num2);
+	} else if (gameType === "multiply") {
+		displayMultiplyQuestion(num1, num2);
+	} else if (gameType === "subtract") {
+		displaySubtractQuestion(num1, num2);
+	} else {
+		alert(`Unknown game type ${gameType}`);
+		throw `Unknown game type ${gameType}, aborting!`;
+	}
 
 }
 
+
+
+function checkAnswer() {
+
+	// Checks the answer against the first element in
+	// the returned calculateCorrectAnswer array
+
+	let userAnswer = parseInt(document.getElementById("answer-box").value);
+	let calculatedAnswer = calculateCorrectAnswer();
+	let isCorrect = userAnswer === calculatedAnswer[0];
+
+	if (isCorrect) {
+		alert("Hey! You got it right! :D");
+		incrementScore();
+	} else {
+		alert(`Awwww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+		incrementWrongAnswer();
+	}
+
+	runGame(calculatedAnswer[1]);
+
+}
 
 /**
  * Get operand from numbers(minus, plus and subtraction)
  * directly from DOM and returns the correct answer
  */
-function checkAnswer() {
-
-}
 
 function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById('operand1').innerText);
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = document.getElementById("operator").innerText;
+
+  if (operator === "+") {
+    return [operand1 + operand2, "addition"];
+  } else {
+    alert(`unimplemented operator ${operator}`);
+    throw `unimplemented operator ${operator}. Aborting!`;
+  }
 
 }
 
